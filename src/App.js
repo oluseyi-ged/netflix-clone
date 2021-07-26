c7f7bf2fd12475d3ce67ec3e6effa484aa9f6936
oluseyi-ged <oluseyi.ged@gmail.com>
2021-07-26 16:42:42 +0100

    got movies and built rows

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css"
+import Row from "./Row"
+import requests from "./request"
 
 function App() {
   return (
     <div className="app">
       <h1>Netflix app</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
